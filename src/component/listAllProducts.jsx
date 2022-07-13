0541e057b64f2237eb2f05849713b611a14eed91
THANG_TRUONG <thang.truong.swin@gmail.com>
2022-07-13 15:37:03 +1000

    badge New Arrial and On Sale

--- src/component/listAllProducts.jsx
+++ src/component/listAllProducts.jsx
@@ -22,22 +22,6 @@ import Cards from "./cards";
 // 	);
 // };
 
-// function ShuffleListProducts(array) {
-// 	let curId = array.length;
-// 	// There remain elements to shuffle
-// 	while (0 !== curId) {
-// 		// Pick a remaining element
-// 		let randId = Math.floor(Math.random() * curId);
-// 		curId -= 1;
-// 		// Swap it with the current element.
-// 		let tmp = array[curId];
-// 		array[curId] = array[randId];
-// 		array[randId] = tmp;
-// 	}
-// 	console.log("ShuffleListProducts clicked");
-// 	return array;
-// }
-
 export function ListAllProducts({ cart, handleClick, handleRemove }) {
 	const [listProducts, setListProducts] = useState([]);
 	// setListProducts(getListProducts());
